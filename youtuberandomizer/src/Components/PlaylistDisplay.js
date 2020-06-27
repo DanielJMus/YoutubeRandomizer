a74053664d1d17bb4f64fa14a3394e1f42fc69aa
DanielJMus <danielmus1999@hotmail.com>
2020-06-28 00:28:50 +1200

    Implementing youtube API, listing playlist videos

--- youtuberandomizer/src/Components/PlaylistDisplay.js
+++ youtuberandomizer/src/Components/PlaylistDisplay.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
-import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import YouTube from '../API/YouTube';
 
 class PlaylistDisplay extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: undefined
+        }
+    }
 
     ListPlaylists() {
         return this.props.playlists.map((item, i) => {
@@ -10,11 +17,37 @@ class PlaylistDisplay extends Component {
         });
     }
 
+    ListVideos (){
+        return this.state.data.items.map((item, i) => {
+            var url = `http://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}`;
+            return (<a href={url}><li key={i}>{item.snippet.title}</li></a>)
+        });
+    }
+
+    GetPlaylist = async (playlistID) => {
+        const response = await YouTube.get('/playlistItems', {
+            params: {
+                playlistId: playlistID,
+                part: 'snippet',
+                maxResults: 50,
+                key: process.env.REACT_APP_API_KEY
+            }
+        })
+        console.log(response.data);
+        this.setState({data: response.data});
+    }
+
+    componentDidMount () {
+        this.GetPlaylist("PLI73u92BTHQDOOypO0Qn8aX1JgLOP2-nD");
+    }
+
     render (){
         const {playlists} = this.props;
+        const {data} = this.state;
         return (
             <ul style={{listStyle:"none"}}>
                 {this.ListPlaylists(playlists)}
+                {data && this.ListVideos()}
             </ul>
         );
     }
